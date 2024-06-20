626ade58175d49143eb6ef384ab3d168fb62a3ac
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-21 00:38:50 +0700

    fetched details and handling pending and errors

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,17 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvent } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const { id } = useParams();
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["event-details"],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
   return (
     <>
       <Outlet />
@@ -11,24 +20,37 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
+      {isError && (
+        <ErrorBlock
+          title="An error occurred"
+          message={error.title?.message || "Failed to fetch event"}
+        />
+      )}
       <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+        {isPending && <LoadingIndicator />}
+        {data && (
+          <>
+            <header>
+              <h1>{data?.title}</h1>
+              <nav>
+                <button>Delete</button>
+                <Link to="edit">Edit</Link>
+              </nav>
+            </header>
+            <div id="event-details-content">
+              <img src={`http://localhost:3000/${data?.image}`} alt="" />
+              <div id="event-details-info">
+                <div>
+                  <p id="event-details-location">{data?.location}</p>
+                  <time dateTime={`Todo-DateT$Todo-Time`}>
+                    {data?.date} @ {data?.time}
+                  </time>
+                </div>
+                <p id="event-details-description">{data?.description}</p>
+              </div>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
+          </>
+        )}
       </article>
     </>
   );
